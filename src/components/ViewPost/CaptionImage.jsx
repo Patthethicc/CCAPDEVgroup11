e5e2c5166784afe59662340af5bf3e6196cea7fd
jjeff <jeff.rigel.cumti@gmail.com>
2025-02-05 19:29:13 +0800

    added comments to profile comment

--- src/components/ViewPost/CaptionImage.jsx
+++ src/components/ViewPost/CaptionImage.jsx
@@ -4,7 +4,7 @@ import ImageContainer from "./ImageContainer";
 export default function CaptionImage() {
   return (
     <>
-      <div class="post-caption-paragraph">
+      <div className="post-caption-paragraph">
         <p id="post-caption">
           This is my current progress on my astronomy map! please let me know
           how it is and what I need to change &lt;3
