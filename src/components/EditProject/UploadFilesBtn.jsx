b3b9d95ab2a9cee5639b9a2a56d6ef2856567615
Patrick Perez <patrick_hans_perez@dlsu.edu.ph>
2025-03-08 02:18:19 +0800

    feat:added edit post

--- src/components/EditProject/UploadFilesBtn.jsx
+++ src/components/EditProject/UploadFilesBtn.jsx
@@ -1,15 +1,27 @@
 import { faPaperclip } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-export default function UploadFilesBtn() {
+export default function UploadFilesBtn({ files, setFiles }) {
+  const handleFileChange = function (e) {
+    setFiles([...e.target.files]);
+  };
+
   return (
-    <button
-      className="bg-[var(--background-color)] rounded-lg py-[0.2rem] mt-3 text-xs w-[20%] shadow-md
+    <div className="sticky">
+      <input
+        type="file"
+        onChange={handleFileChange}
+        className="absolute inset-0 w-[20%] h-[20%] opacity-0 cursor-pointer"
+      />
+      <button
+        className="bg-[var(--background-color)] rounded-lg py-[0.2rem] mt-3 text-xs w-[20%] shadow-md
         focus-within:border-[var(--field-border-color)] hover:bg-[var(--brighter-bg-color)] transition-colors
         border-solid border-2 border-transparent font-bold"
-    >
-      <FontAwesomeIcon icon={faPaperclip} className="mx-1 " />
-      Upload
-    </button>
+      >
+        <FontAwesomeIcon icon={faPaperclip} className="mx-1 " />
+        Upload
+      </button>
+    </div>
   );
 }
+
--- src/pages/EditProject.jsx
+++ src/pages/EditProject.jsx
@@ -1,10 +1,87 @@
 import TitleField from "../components/EditProject/TitleField.jsx";
-import StsDropdownMenu from "../components/EditProject/StsDropdownMenu.jsx";
 import BodyField from "../components/EditProject/BodyField.jsx";
 import UploadFilesBtn from "../components/EditProject/UploadFilesBtn.jsx";
-import PostBtn from "../components/EditProject/PostBtn.jsx";
+import EditBtn from "../components/EditProject/EditBtn.jsx";
+import SliderProgress from "../components/EditProject/SliderProgress.jsx";
+import API from "../url.js";
+import { useState, useEffect } from "react";
+import { useParams } from 'react-router-dom';
+import "../App.css";
 
 export default function EditProject() {
+  const _id = useParams();
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+  const [progress, setProgress] = useState("Status");
+  const [deadlength, setDeadLength] = useState(0);
+  const [post, setPost] = useState(false);
+
+  useEffect(
+    function () {
+      if (!post) return;
+
+      const editData = async function () {
+        const data = {
+          title,
+          content: body,
+          deadline: {
+            progress,
+            deadline_length: deadlength,
+          },
+          created_at: Date.now(),
+          upvotes: 0,
+          downvotes: 0,
+          comment_ids: [],
+        };
+
+        try {
+          const response = await fetch(`${API}/${_id}`, {
+            method: "PUT",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(data),
+          });
+
+          if (!response.ok) {
+            throw new Error("Error editing post.");
+          }
+
+          const result = await response.json();
+          console.log(result);
+
+        } catch (err) {
+          console.error("Error editing data: " + err.message);
+        } finally {
+          setPost(false);
+        }
+      };
+
+      editData();
+    },
+    [_id, title, body, progress, deadlength, post],
+  );
+
+  useEffect(
+    function () {
+      adjustDeadLength(progress);
+    },
+    [progress],
+  );
+
+  const handlePost = function () {
+    setPost(true);
+  };
+
+  const adjustDeadLength = function (progress) {
+    const map = {
+      Started: 0,
+      "In Progress": 50,
+      Finished: 100,
+      Deployed: 100,
+    };
+
+    setDeadLength(map[progress] || 0);
+  };
+
   return (
     <div
       className="create-project flex px-8 py-6 flex-col 
@@ -12,11 +89,16 @@ export default function EditProject() {
       bg-[var(--primary-color)] box-border rounded-lg gap-4 shadow-md"
     >
       <h1 className="font-bold text-xl">Edit Project</h1>
-      <TitleField value="My Astrology Chart" />
-      <StsDropdownMenu value="In Progress" />
-      <BodyField value="This is my current progress on my astronomy map! please let me know how it is and what I need to change <3" />
-      <UploadFilesBtn />
-      <PostBtn />
+      <TitleField title={title} setTitle={setTitle} />
+            <SliderProgress
+              progress={progress}
+              setProgress={setProgress}
+              deadlength={deadlength}
+              setDeadLength={setDeadLength}
+            />
+            <BodyField body={body} setBody={setBody} />
+            <UploadFilesBtn />
+            <EditBtn handlePost={handlePost} />
     </div>
   );
 }
