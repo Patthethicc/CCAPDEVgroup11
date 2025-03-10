8b3fabdf66e37ce5c441cdf961bd84decef6c16d
Solenad <roe.dizon80@gmail.com>
2025-03-10 11:34:37 +0800

    chore: added text for error handling

--- src/pages/LogInPage.jsx
+++ src/pages/LogInPage.jsx
@@ -5,18 +5,18 @@ import LogInForm from "../components/LogIn&SignUp/LogInForm";
 import LandingHeader from "../components/Landing/LandingHeader";
 
 export default function LogInPage() {
-    return (
-        <div className="login-page">
-            <LandingHeader />
-            <Logo />
-            <div className="main2">
-                <div className="outerpad">
-                    <div className="innerpad">
-                        <LogInText />
-                        <LogInForm />
-                    </div>
-                </div>
-            </div>
+  return (
+    <div className="login-page">
+      <LandingHeader />
+      <Logo />
+      <div className="main2">
+        <div className="outerpad">
+          <div className="innerpad">
+            <LogInText />
+            <LogInForm />
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
